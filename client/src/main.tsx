05fe728a59d52449f4565ea35f24b31994e60cfa
Peter Blanco <petercblanco@gmail.com>
2025-03-22 16:06:56 -0400

    Setup amplify

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Amplify } from "aws-amplify";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import config from "../amplify_outputs.json";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+Amplify.configure(config);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
